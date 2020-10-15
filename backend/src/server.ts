568c6143b96360e3d0ad5f326071621940807a5c
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-10-14 23:57:47 -0300

    Configurando banco de dados

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,29 +1,12 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
 
 app.use(express.json());
 
-// Rota = conjunto
-// Recurso = usuário
-
-// Métodos HTTP = GET, POST, PUT, DELETE
-// Parâmetros
-
-// GET = Buscar uma informação (Lista, Item)
-// POST = Criando uma informação
-// PUT = Editando uma informação
-// DELETE = Deletando uma infomação
-
-// Query Params: http://localhost:3333/users?search=eduardo
-// Route Params: http://localhost:3333/users/1 (Identificar um recurso)
-// Body: http://localhost:3333/users
-
-app.post('/users/:id', (request, response) => {
-    console.log(request.query);
-    console.log(request.params);
-    console.log(request.body);
-    
+app.get('/users', (request, response) => {
     return response.json({ message: "Hello World" });
 });
 
