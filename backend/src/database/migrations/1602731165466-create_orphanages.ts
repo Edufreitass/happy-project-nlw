51c30a088fc7edf8bda598e874a84f48c84cc5fd
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-10-15 22:48:26 -0300

    Criando orfanato sem imagem

--- backend/src/database/migrations/1602731165466-create_orphanages.ts
+++ backend/src/database/migrations/1602731165466-create_orphanages.ts
@@ -41,6 +41,10 @@ export class createOrphanages1602731165466 implements MigrationInterface {
             type: 'text',
           },
           {
+            name: 'opening_hours',
+            type: 'varchar',
+          },
+          {
             name: 'open_on_weekends',
             type: 'boolean',
             default: false
